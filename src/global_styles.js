739d42078cdd653e2755bd1198dc8e065c1f987d
user <user@Mac16.local>
2019-03-01 17:41:11 +0700

    global styles

---
+++ src/global_styles.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  button, a, p, h1,
+  h2, h3, h4, h5,
+  ul, li, img,
+  input {
+    margin: 0;
+    padding: 0;
+  }
+
+  button, img {
+    user-select: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  @keyframes circle {
+    0%, 100% {
+      animation-timing-function: cubic-bezier(0.5, 0, 1, 0.5);
+    }
+    0% {
+      transform: rotateY(0deg);
+    }
+    50% {
+      transform: rotateY(1800deg);
+      animation-timing-function: cubic-bezier(0, 0.5, 0.5, 1);
+    }
+    100% {
+      transform: rotateY(3600deg);
+    }
+  }
+`;
