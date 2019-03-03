b71d192eb3d208f8f77134d05641554c82f92ad0
StudenovRoman <rstudenov@allmax.team>
2019-03-03 11:20:12 +0700

    change api

--- src/store/search/helpers/api.js
+++ src/store/search/helpers/api.js
@@ -1,3 +1,6 @@
 import axios from 'axios';
 
-export const searchMovie = title => axios.get(`http://www.omdbapi.com/?apikey=1977b733&t=${title}`).then(res => res);
+export const searchMovie = (title) => {
+  if (title.startsWith('tt')) return axios.get(`http://www.omdbapi.com/?apikey=1977b733&i=${title}`).then(res => res); 
+  return axios.get(`http://www.omdbapi.com/?apikey=1977b733&t=${title}`).then(res => res);
+}
