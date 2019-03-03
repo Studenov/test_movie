b1018a471f6a6e5349d4b340ccad7354d7a9c2c7
StudenovRoman <rstudenov@allmax.team>
2019-03-03 11:20:22 +0700

    change router

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import { SearchConnect } from 'components/search';
+import { MovieConnect } from 'components/movie';
 
 export const Navigator = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={SearchConnect} />
-      <Route path="/movie/:id" component={SearchConnect} />
+      <Route path="/" exact component={SearchConnect} />
+      <Route path="/:id" component={MovieConnect} />
       <Redirect to="/" />
     </Switch>
   </BrowserRouter>
